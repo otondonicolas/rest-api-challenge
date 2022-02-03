1695e3c2f62cc2ed51f0b91c55f1cf37fea638e2
Nicolas Otondo <otondonicolas@gmail.com>
2022-02-03 20:47:20 -0300

    added some tests as an example, one model to take a look to open pojo dependency and how it works, and the service impl as a most important class to test with mockito.

--- src/test/java/com/example/api_store/model/dto/ProductDTOTest.java
+++ src/test/java/com/example/api_store/model/dto/ProductDTOTest.java
@@ -1,4 +1,42 @@
+package com.example.api_store.model.dto;
+
+import com.openpojo.reflection.filters.FilterPackageInfo;
+import com.openpojo.reflection.impl.PojoClassFactory;
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.rule.impl.GetterMustExistRule;
+import com.openpojo.validation.rule.impl.SetterMustExistRule;
+import com.openpojo.validation.test.impl.GetterTester;
+import com.openpojo.validation.test.impl.SetterTester;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ProductDTOTest {
-  
+
+    @Test
+    void productDtoPojoValidator() {
+
+        final Validator pojoValidator =
+                ValidatorBuilder.create()
+                        .with(new GetterMustExistRule())
+                        .with(new GetterTester())
+                        .with(new SetterTester())
+                        .build();
+
+        pojoValidator.validate(PojoClassFactory.getPojoClass(ProductDTO.class));
+
+        assertNotNull(pojoValidator);
+    }
+
+    @Test
+    void productDtoTest() {
+        ProductDTO productDTO = ProductDTO.builder().build();
+        productDTO.toString();
+
+        ProductDTO productDtoNoArgs = new ProductDTO();
+
+        assertNotNull(productDTO);
+        assertNotNull(productDtoNoArgs);
+    }
 }
--- src/test/java/com/example/api_store/service/impl/StoreServiceImplTest.java
+++ src/test/java/com/example/api_store/service/impl/StoreServiceImplTest.java
@@ -1,4 +1,77 @@
+package com.example.api_store.service.impl;
+
+import com.example.api_store.adapter.ProductAdapter;
+import com.example.api_store.model.dto.ApiStoreRequestDto;
+import com.example.api_store.model.dto.ProductDTO;
+import com.example.api_store.model.entity.ProductEntity;
+import com.example.api_store.repository.IStoreRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.HttpServerErrorException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+
+@ExtendWith(SpringExtension.class)
 class StoreServiceImplTest {
-  
+
+    @InjectMocks
+    private StoreServiceImpl storeService;
+
+    @Mock
+    private IStoreRepository repository;
+
+    @Mock
+    private ProductAdapter adapter;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void findAllProducts() {
+        List<ProductEntity> productEntities = new ArrayList<>();
+        List<ProductDTO> productDTOS = new ArrayList<>();
+
+        doReturn(productEntities).when(repository).findAll();
+        doReturn(productDTOS).when(adapter).entityListToDtoList(productEntities);
+
+        assertEquals(productDTOS, storeService.findAllProducts());
+    }
+
+    @Test
+    void findProductById() throws Exception {
+        ProductDTO productDto = ProductDTO.builder().build();
+        Optional<ProductEntity> productEntityOptional = Optional.of(new ProductEntity());
+
+        doReturn(productEntityOptional).when(repository).findById(anyLong());
+        doReturn(productDto).when(adapter).entityToDto(productEntityOptional.get());
+
+        assertEquals(productDto, storeService.findProductById(anyLong()));
+    }
+
+    @Test
+    void deleteProductById() throws Exception {
+        //I didn't expend so much time here because deleteById method is a void method.
+        assertTrue(storeService.deleteProductById(anyLong()));
+    }
+
+    @Test
+    void createProduct() throws Exception {
+        ApiStoreRequestDto apiStoreRequestDto = ApiStoreRequestDto.builder().build();
+        assertTrue(storeService.createProduct(apiStoreRequestDto));
+    }
 }
